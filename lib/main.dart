b75aa0f3e57fe2909fa907dd17dbd42461c789cc
Uldana Rakhmetova <117116380+zzmeyuka@users.noreply.github.com>
2025-04-11 00:22:50 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,1 @@
-import 'package:flutter/material.dart';
-import 'about_page.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: AboutPage(),
-    theme: ThemeData(primarySwatch: Colors.teal),
-  ));
-}
